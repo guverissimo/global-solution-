eb19a81ab33a69f07fafd79cf6c72860f64f33d3
Gustavo Verissimo <gustavo.verissimo1@outlook.com>
2023-11-21 12:34:22 -0300

    add dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Dashboard = () => {
   const [usuarioLogado] = useState(
     JSON.parse(sessionStorage.getItem("usuarioLogado"))
   );
 
+  const [estoque, setEstoque] = useState([]);
+
   const handleLogout = () => {
     sessionStorage.removeItem("usuarioLogado");
-    window.location.reload();
+    window.location = "/";
   };
+
+  useEffect(() => {
+    fetch("http://localhost:5000/usuarios")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        for (let index = 0; index < data.length; index++) {
+          const element = data[index];
+          // console.log(element);
+          if (element.usuario == usuarioLogado.usuario) {
+            setEstoque(element.estoque.remedios);
+          }
+        }
+      });
+  }, []);
+
+  console.log(estoque);
+  for (let index = 0; index < estoque.length; index++) {
+    const element = estoque[index];
+    console.log(element);
+  }
 
   return (
     <>
@@ -17,8 +42,18 @@ const Dashboard = () => {
           {usuarioLogado != null ? `Cliente: ${usuarioLogado.usuario}` : ""}
         </p>
         <button onClick={handleLogout}>Logout</button>
+        <div>
+          {estoque.map((r) => {
+            return (
+              <>
+                <div key={r.nome}>
+                  <p>{r.nome}</p>
+                </div>
+              </>
+            );
+          })}
+        </div>
       </div>
-      
     </>
   );
 };
