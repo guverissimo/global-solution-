66577deb7438f645b016a769263b3bd8988a59ac
Gustavo Verissimo <gustavo.verissimo1@outlook.com>
2023-11-21 14:47:35 -0300

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import {} from 'react'
-import Navegacao from './components/Navegacao'
-import { Outlet } from 'react-router-dom'
-import Rodape from './components/Rodape'
+import {} from "react";
+import Navegacao from "./components/Navegacao";
+import { Outlet } from "react-router-dom";
+import Rodape from "./components/Rodape";
 
 function App() {
-
   return (
     <>
-     <Navegacao />
-     <Outlet />
-     <Rodape />
+      <div className="app-container">
+        <Navegacao className="navegacao" />
+        <Outlet className="main" />
+        <Rodape className="footer" />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,40 @@
 import { useEffect, useState } from "react";
 
 const Dashboard = () => {
+  const [allData, setAllData] = useState()
   const [usuarioLogado] = useState(
     JSON.parse(sessionStorage.getItem("usuarioLogado"))
   );
-
+  const [userID, setUserID] = useState()
   const [estoque, setEstoque] = useState([]);
+  const [novo, setNovo] = useState({ ...estoque,
+    nome: '',
+    qtd: ''
+  })
 
   const handleLogout = () => {
     sessionStorage.removeItem("usuarioLogado");
     window.location = "/";
   };
+  // Adicionar Novo Remedio
+  const handleChange =(e)=>{
+    setNovo({...novo, [e.target.name]:e.target.value});
+  };
+
+  // Adicionar Novo Remedio
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+
+    fetch(`http://localhost:5000/usuarios/${userID}`,{
+      method:'post',
+      headers:{
+          'Content-Type':'application/json',
+      },
+      body:JSON.stringify(novo),
+    }).then(()=>{
+      window.location='/dashboard';
+    });
+  }
 
   useEffect(() => {
     fetch("http://localhost:5000/usuarios")
@@ -24,17 +48,14 @@ const Dashboard = () => {
           // console.log(element);
           if (element.usuario == usuarioLogado.usuario) {
             setEstoque(element.estoque.remedios);
+            setUserID(element.id)
+            setAllData(element)
           }
         }
       });
   }, []);
 
-  console.log(estoque);
-  for (let index = 0; index < estoque.length; index++) {
-    const element = estoque[index];
-    console.log(element);
-  }
-
+  console.log(allData);
   return (
     <>
       <div>
@@ -42,18 +63,57 @@ const Dashboard = () => {
           {usuarioLogado != null ? `Cliente: ${usuarioLogado.usuario}` : ""}
         </p>
         <button onClick={handleLogout}>Logout</button>
-        <div>
+      </div>
+      <div>
+
+<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  Adicionar Remedio
+</button>
+<div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h1 className="modal-title fs-5" id="staticBackdropLabel">Adicionar Remedio</h1>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input 
+            type="text" 
+            name="nome"
+            value={novo.nome}
+            placeholder="Nome do medicamento"
+            onChange={handleChange}
+            />
+            <input 
+            type="text" 
+            name="qtd"
+            value={novo.qtd}
+            placeholder="Quantidade do medicamento"
+            onChange={handleChange}
+            />
+          </div>
+        <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" className="btn btn-primary">Adicionar</button>
+      </div>
+        </form>
+      </div>
+      
+    </div>
+  </div>
+</div>
+      </div>
+      <div> 
           {estoque.map((r) => {
             return (
               <>
-                <div key={r.nome}>
-                  <p>{r.nome}</p>
-                </div>
+                <p key={r.nome}>{r.nome}</p>
               </>
             );
           })}
         </div>
-      </div>
     </>
   );
 };
